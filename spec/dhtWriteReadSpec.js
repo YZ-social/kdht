10b01d8499d0dd134f1dbc318905af8c3d776425
Howard Stearns <howard.stearns@gmail.com>
2026-01-10 14:31:45 -0800

    include webrtc tests

--- spec/dhtWriteReadSpec.js
+++ spec/dhtWriteReadSpec.js
@@ -1,28 +1,38 @@
 #!/usr/bin/env npx jasmine
 const { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } = globalThis; // For linters.
 import process from 'node:process';
-import { exec } from 'node:child_process';
+import { spawn } from 'node:child_process';
 import { v4 as uuidv4 } from 'uuid';
 import { WebContact, Node } from '../index.js';
-
-// I cannot get yargs to work properly within jasmine. Get args by hand.
-// Note: jasmine will treat --options as arguments to itself. To pass them to the script, you need to separate with '--'.
-const nWritesIndex = process.argv.indexOf('--nWrites');
-const baseURLIndex = process.argv.indexOf('--baseURL');
-const waitBeforeReadIndex = process.argv.indexOf('--waitBeforeRead');
-const verboseIndex = process.argv.indexOf('--verbose');
-const shutdownIndex = process.argv.indexOf('--shutdown');
-
-const nWrites = nWritesIndex >= 0 ? JSON.parse(process.argv[nWritesIndex + 1]) : 10;
-const baseURL = baseURLIndex >= 0 ? process.argv[baseURLIndex + 1] : 'http://localhost:3000/kdht';
-const waitBeforeRead = waitBeforeReadIndex >= 0 ? JSON.parse(process.argv[waitBeforeReadIndex + 1]) : 10;
-const verbose = verboseIndex >= 0 ? JSON.parse( process.argv[verboseIndex + 1] || 'true' ) : false;
-const shutdown = shutdownIndex >= 0 ? JSON.parse( process.argv[shutdownIndex + 1] || 'true' ) : true;
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 describe("DHT write/read", function () {
-  let contact;
+  let contact, portalProcess;
+  const verbose = false;
+  const baseURL = 'http://localhost:3000/kdht';
+  const nPortals = 10;
+  const nBots = 10;
+  const fixedSpacing  = 2; // Between portals.
+  const variableSpacing = 5; // Additional random between portals.
+  const nWrites = 40;
+  const waitBeforeRead = 15e3;
+  const thrash = true;
+
+  
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const portalSeconds = fixedSpacing * nPortals + 1.5 * variableSpacing;
+  const botsMilliseconds = 2 * Node.refreshTimeIntervalMS;
+  
   beforeAll(async function () {
+    portalProcess = spawn('node', [path.resolve(__dirname, 'portal.js'), '--nPortals', nPortals, '--nBots', nBots, '--thrash', thrash.toString(), '--verbose', verbose.toString()]);
     contact = await WebContact.create({name: uuidv4(), debug: verbose});
+    console.log('starting portals over', portalSeconds, 'seconds');
+    await Node.delay(portalSeconds * 1e3);
+    console.log('starting bots over', botsMilliseconds/1e3, 'seconds');    
+    await Node.delay(botsMilliseconds);
+
     const bootstrapName = await contact.fetchBootstrap(baseURL);
     const bootstrapContact = await contact.ensureRemoteContact(bootstrapName, baseURL);
     console.log(new Date(), contact.sname, 'joining', bootstrapContact.sname);
@@ -39,12 +49,8 @@ describe("DHT write/read", function () {
     console.log(new Date(), 'Reading');
   }, 5e3 * nWrites + 2 * Node.refreshTimeIntervalMS);
   afterAll(async function () {
-    if (shutdown) {
-      contact.disconnect();
-      exec('pkill kdht-portal-server');
-    } else {
-      contact.disconnect();
-    }
+    contact.disconnect();
+    portalProcess.kill();
   });
   for (let index = 0; index < nWrites; index++) {
     it(`reads ${index}.`, async function () {
