5e2d279136e6c351f561d24fad9a8713cf452113
Howard Stearns <howard.stearns@gmail.com>
2026-02-05 11:53:23 -0800

    forgot files

---
+++ nodes/storageBag.js
@@ -0,0 +1,117 @@
+// A StorageBag instance is the in-memory storage for a specific DHT key.
+//
+// The DHT replicates this instance among the closest nodes to the key.
+// The DHT does this by determining the appropriate Contacts (via locateNodes)
+// and sending the store(key, storageBag) RPC to each of those Contacts.
+// Sending the RPC will involve JSON.stringify(storageBag) for transport on the wire.
+//
+// (An application might produce this key as a hash of some string - perhaps a
+// string of multiple parts such as "scope:event". Or it might be a key of multiple
+// parts so as to be located within a region of the DHT address space.)
+//
+// Each storageBag may have several values added (or cancelled) at different times.
+// We refer to each item in a storageBag collection as a storageItem.
+// At any given moment, a storing nodes's copy of the storageBag might have an
+// incomplete picture of all the storageItems, and thus the receiving node merges
+// the new bag item-by-item with any existing items.
+// Each item can be created by the application with three properties that
+// go along with the item payload: type, subject, and issuedTime.
+// - Different types can have different merge rules. E.g., corresponding to
+//   mutable vs immutable, or by smart contract, etc.
+//   A single bag might have items of multiple types that appear togther in the same
+//   storageBag, such as published messages and subscriptions.
+// - Each storageItem is individually expired, based on when it was created
+//   and the expiration time for that type of storageItem.
+// - A storageItem's expiration can be extended for the same or for a different
+//   payload, by specifying a new issuedTime or a new payload on the same subject.
+//   The subject can be specified so that we know which item to moddify. 
+//   The subject defaults to be the same as the payload, but applications might
+//   use a per-user or per-message GUID.
+
+export class StorageBag {
+
+  types = {}; // {[type]: {[subject]: storageItem, ...}, ...}
+
+  // Recall that toJSON() does not return a string, but rather a replacement object to be further stringified.
+  toJSON() { // Answer [ pojo, ...], where each pojo are the properties of a StorageItem
+    let list = [];
+    Object.values(this.types).forEach(typeStorageItems => 
+      Object.values(typeStorageItems).forEach(storageItem => storageItem.isCancelled || list.push(storageItem.toJSON())));
+    return list;
+  }
+  toString() {
+    const rawSubjects = Object.values(this.types.raw);
+    if (rawSubjects.length === 1) return JSON.stringify(rawSubjects[0].payload);
+    return super.toString();
+  }
+  merge(storageItems, node, key) { // Add each allowed storageItem of serialized to the organized types if allowed, including any side-effects.
+    // Returns merged storageBag.
+    const now = Date.now();
+    for (const storageItem of storageItems) {
+      const proposed = StorageItem.create(storageItem);
+      const subjects = this.types[proposed.type] ||= {};
+      proposed.merge1(subjects, now, this, node, key);
+    }
+    return this;
+  }
+}
+
+// Example merge rules:
+// Don't allow times in the future.
+// Require signature issuer to match existing. (When we have JWS storageItems)
+// Require subject to match hash of payload (or default it).
+// Keep only the newest of a subject.
+// Keep only the oldest of a subject.
+// Keep all unexpired of a subject.
+
+export class StorageItem {
+  constructor({payload, subject = payload.toString(), issuedTime = Date.now(), type = this.constructor.type, expiration = Infinity, ...rest}) {
+    // TODO: accept and cache a JWS and have getters that extract these same three parts.
+    expiration = Math.min(expiration, this.constructor.expiration);
+    Object.assign(this, {subject, issuedTime, payload, type, expiration, ...rest});
+  }
+  static type = 'raw';
+  static expiration = 5 * 60e3; // five minutes for development
+  // Applications can extend the set of recognized types, although there is not YET a mechanism for nodes to dynamically load defs on-demand.
+  static storageItems = {}; // Maps type => class
+  static register() { // Add to storageItems.
+    this.storageItems[this.type] = this;
+  }
+  static create(storageItemProperties) {
+    const kind = StorageItem.storageItems[storageItemProperties.type || 'raw'];
+    return new kind(storageItemProperties);
+  }
+  get isCancelled() {
+    // Note: There can be type-specific "removed" markers, but we don't actually delete those,
+    // because a merge would add them back.
+    return this.payload === null;
+  }
+  toJSON() {
+    if (this.isCancelled) return undefined;
+    const {type, subject, issuedTime, payload} = this;
+    return {type, subject, issuedTime, payload};
+  }
+  merge1(subjects, now) { // Add this into subjects if allowed and return this, else null.
+    // where subjects is: subjectName => storageItem
+
+    const {type, subject, payload, issuedTime, expiration} = this;
+    let {issuedTime:existingTime = 0, timer} = subjects[subject] || {};
+
+    if (!this.allowedTime(existingTime, now, issuedTime)) return null;
+        
+    const timeout = issuedTime + expiration - now;
+    clearTimeout(timer);
+    this.timer = setTimeout(() => delete subjects[subject], timeout);
+
+    subjects[subject] = this;
+    return this;
+  }
+  allowedTime(existingTime, now, issuedTime) { // Keep only the latest unexpired
+    if (issuedTime > now) return false; // Cannot stake out the future. TODO: allow some clock skew.
+    if (issuedTime <= existingTime) return false;
+    return true;
+  }
+}
+StorageItem.register();
+
+
