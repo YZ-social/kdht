04edd69be529f816cc6e760afd5891be11a480a4
Howard Stearns <howard.stearns@gmail.com>
2026-03-14 10:57:09 -0700

    reduce "large" test

--- spec/dhtSimulationsSpec.js
+++ spec/dhtSimulationsSpec.js
@@ -195,6 +195,6 @@ describe("DHT", function () {
   // With disconnects:
   test({pingTimeMS: 0, refreshTimeIntervalMS: 5e3, notes: "Small-network thrashing"});
   test({notes: "Normal ops"});
-  test({setupTimeMS: 40e3, notes: "Large-network thrashing"});
+  test({setupTimeMS: 30e3, notes: "Large-network thrashing"});
   //test({maxTransports: 15, notes: "Limited connections on thrashing."}); // See comment for "Meansingful maxTransports, above.
 });
